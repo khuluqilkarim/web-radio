aef34397de413d4bd9dfe3c7855e464cdb1c91ab
khuluqilkarim <mpherex24@gmail.com>
2023-10-25 23:48:33 +0700

    update index

--- main.js
+++ main.js
@@ -3,86 +3,31 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-let filter = {
-  limit: 5, // list max 5 items
-  by: "tag", // search in tag
-  searchterm: "jazz", // term in tag
-};
-RadioBrowser.getStations(filter)
-  .then((data) => {
-    app.get("/", (req, res) => {
-      let cards = ""; // Initialize an empty string to store the card HTML
+app.get("/radio/:limit/:by/:tag", (req, res) => {
+  const limit = req.params.limit;
+  const by = req.params.by;
+  const tag = req.params.tag;
 
-      // Iterate over the data and generate a card for each item
-      data.forEach((station, i) => {
-        cards += `
-          <div class="card">
-            <img src="${station.favicon}" alt="Image ${i + 1}" />
-            <h3>${station.name}</h3>
-            <p>${station.tags}</p>
-            <button class="play-button">Play</button>
-          </div>`;
-      });
+  let filter = {
+    limit: parseInt(limit),
+    by: by,
+    searchterm: tag,
+  };
 
-      // Send the HTML containing all the cards
-      res.send(`
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <title>Play Radio</title>
-          <style>
-            body {
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              height: 100vh;
-              margin: 0;
-            }
-
-            .card {
-              width: 300px;
-              border: 1px solid #ccc;
-              border-radius: 5px;
-              padding: 15px;
-              margin: 10px;
-              text-align: center;
-              box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-            }
-
-            .card h3 {
-              font-size: 1.5rem;
-            }
-
-            .card p {
-              font-size: 1rem;
-            }
-
-            .play-button {
-              background-color: #007bff;
-              color: #fff;
-              border: none;
-              padding: 10px 20px;
-              border-radius: 5px;
-              cursor: pointer;
-            }
-
-            .card img {
-              max-width: 100%;
-              height: auto;
-            }
-          </style>
-        </head>
-        <body>
-          ${cards} <!-- Insert the generated cards here -->
-        </body>
-        </html>
-      `);
-    });
-  })
-  .catch((error) => console.error(error));
+  RadioBrowser.getStations(filter)
+    .then((data) => {
+      const stations = data.map((stations) => ({
+        nama: stations.name,
+        url: stations.url,
+        url_resolve: stations.url_resolved,
+        favicon: stations.favicon,
+      }));
+      res.send(stations);
+      console.log(stations.length);
+    })
+    .catch((error) => console.error(error));
+});
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}/radio/5/tag/jazz`);
 });
